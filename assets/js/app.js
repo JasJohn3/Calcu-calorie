6773daab04e6c75f694db1a1dfc9076e0201e7f1
Your Name <jasjohn3@uat.edu>
2021-07-29 04:57:33 -0700

    Added js script for Materialize instances

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+    var elems = document.querySelectorAll('.tap-target');
+    var instances = M.TapTarget.init(elems);
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
   });
